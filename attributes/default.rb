a52c75cd7c885d67d2efefb81dd921e0f11ed862
seph <seph@directionless.org>
2011-03-15 12:24:51 -0400

    settings for postgresql.conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -68,6 +68,40 @@ else
 end
 
 # default to allowing no extra hosts in the pg_hba
-set[:postgresql][:md5hosts] = []
-set[:postgresql][:identhosts] = []
-set[:postgresql][:idententries] = []
+default[:postgresql][:md5hosts] = []
+default[:postgresql][:identhosts] = []
+default[:postgresql][:idententries] = []
+
+# postgresql.conf settings
+# Things set to nil won't show up, and will take defaults
+default[:postgresql][:listen_address] = nil
+default[:postgresql][:max_connections] = 100
+default[:postgresql][:tcp_keepalives_idle] = nil
+default[:postgresql][:tcp_keepalives_interval] = nil
+default[:postgresql][:tcp_keepalives_count] = nil
+default[:postgresql][:shared_buffers] = "24MB"
+default[:postgresql][:temp_buffers] = nil
+default[:postgresql][:max_prepared_transactions] = nil
+default[:postgresql][:work_mem] = nil
+default[:postgresql][:maintenance_work_mem] = nil
+default[:postgresql][:max_stack_depth] = nil
+default[:postgresql][:effective_io_concurrency] = nil
+default[:postgresql][:no_fsync] = false  # disable fsync? Good for development
+default[:postgresql][:wal_buffers] = nil
+default[:postgresql][:checkpoint_segments] = nil
+default[:postgresql][:checkpoint_timeout] = nil
+default[:postgresql][:checkpoint_completion_target] = nil
+default[:postgresql][:checkpoint_warning] = nil
+default[:postgresql][:archive_mode] = nil
+default[:postgresql][:archive_command] = nil
+default[:postgresql][:archive_timeout] = nil
+default[:postgresql][:random_page_cost] = nil
+default[:postgresql][:effective_cache_size] = nil
+default[:postgresql][:log_destination] = nil
+default[:postgresql][:syslog_facility] = nil
+default[:postgresql][:syslog_ident] = nil
+default[:postgresql][:log_min_duration_statement] = nil
+default[:postgresql][:log_duration] = nil
+default[:postgresql][:log_line_prefix] = '%t '
+default[:postgresql][:autovacuum] = nil
+default[:postgresql][:log_autovacuum_min_duration] = nil
