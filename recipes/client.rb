890df2882a65deb49da71fdb505838c5b064e219
Dan (Apollo) Catlin <dan@actbluetech.com>
2018-05-22 09:20:29 -0400

    modify pgdg published 9.6.9 version

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,4 +1,4 @@
 package "postgresql-client-#{node['postgresql']['version']}" do
-  version "#{node['postgresql']['version_full']}-1.pgdg14.04+1"
+  version "#{node['postgresql']['version_full']}-2.pgdg14.04+1"
   action :install
 end
