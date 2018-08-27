68ca7471008bd2e38b113f08fb23073f4e331f45
Apollo Catlin <apollo.catlin@gmail.com>
2018-08-27 16:59:56 -0400

    allow downgrades in postgres client install

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,4 +1,5 @@
 package "postgresql-client-#{node['postgresql']['version']}" do
   version "#{node['postgresql']['version_full']}.pgdg14.04+1"
+  options '--allow-downgrades'
   action :install
 end
