f087e841c908e90df0977752ab5dade0bc4bd3c0
Apollo Catlin <dan@actbluetech.com>
2019-05-19 05:53:08 -0400

    fix setting format

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'ops@actbluetech.com'
 license           'Proprietary - All Rights Reserved'
 description       'Installs and configures postgresql for clients or servers'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           '0.15.0'
+version           '0.16.0'
 issues_url        'https://github.com/actblue/chef/issues'
 source_url        'https://github.com/actblue/chef'
 chef_version      '~> 12' if respond_to?(:chef_version)
